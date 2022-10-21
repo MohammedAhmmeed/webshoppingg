d9c1d25055f0441a87a837a0ab7cfae147d3a041
Your Name <mohamedhh3447@gmail.com>
2022-10-21 10:45:30 +0200

    filter-com

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,10 @@
 import React , {useState} from "react";
+import Filter from "./Components/Filter/Filter";
 import Footer from "./Components/Footer/Footer";
 import Headar from "./Components/headar/Headar";
 import Products from "./Components/Products/Products";
 import data from "./data.json"
+import './index.css'
 
 
 function App() {
@@ -14,8 +16,8 @@ function App() {
 <Headar /> 
  <main>
   <div className="wrapper" >
-<Products products={products}/>
-    <div className="filter">Filter</div>
+  <Products className="product" products={products}/>
+   <Filter className="filer" />
   </div>
  </main>
 <Footer />
